e7abb0440f87b2021bf89d1ec12954efeacdd78b
Rhett Flanagan <rhettsfp@gmail.com>
2020-08-16 12:23:40 +0200

    Ran an early trial on the new sim. Results are somewhat jank, but it works pending improvement.

--- src/main/java/train/Main.java
+++ src/main/java/train/Main.java
@@ -3,11 +3,16 @@ package train;
 
 import intersectionmanagement.trial.Trial;
 import org.apache.commons.lang3.SerializationUtils;
+import org.encog.Encog;
 import org.encog.ml.ea.train.EvolutionaryAlgorithm;
 import org.encog.neural.neat.NEATNetwork;
 import org.encog.neural.neat.NEATPopulation;
 import org.encog.neural.neat.NEATUtil;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -20,28 +25,46 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
+        // Retrieve parameters
         String parameters = new String(Files.readAllBytes(Paths.get(args[0])), StandardCharsets.UTF_8);
         int iterations = Integer.parseInt(args[1]);
         int popSize = Integer.parseInt(args[2]);
         int sims = Integer.parseInt(args[3]);
 
+        // Setup trainer
         NEATPopulation population = new NEATPopulation(14, 2, popSize);
+        population.setInitialConnectionDensity(1.0);// not required, but speeds training
+        population.reset();
         CarControllerScore score = new CarControllerScore(parameters, sims);
 
-        EvolutionaryAlgorithm train = NEATUtil.constructNEATTrainer(population, score);
-
+        // Train
+        final EvolutionaryAlgorithm train = NEATUtil.constructNEATTrainer(population, score);
         for(int i = 0; i < iterations; i++){
             train.iteration();
             System.out.println("Epoch #" + train.getIteration() + " Error:" + train.getError()+ ", Species:" + population.getSpecies().size());
         }
 
+        // Retrieve and save the best network
         NEATNetwork network = (NEATNetwork)train.getCODEC().decode((train.getBestGenome()));
-
         Trial trial = new Trial(parameters, SerializationUtils.serialize(network));
         for(int i = 0; i < 10; i++){
             trial.setupSim();
             int collisions = trial.runSimulation();
             System.out.println("Run " + (i + 1) + " collisions: " + collisions);
         }
+
+        // Save the network and parameters
+        JSONObject jsonParameters = new JSONObject(parameters);
+        jsonParameters.remove("neural_network");
+        jsonParameters.put("neural_network", new JSONArray(SerializationUtils.serialize(network)));
+
+        try(FileWriter file = new FileWriter(args[4])){
+            file.write(jsonParameters.toString());
+            file.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Encog.getInstance().shutdown();
     }
 }
