79527367ac85f066a1fea7bbee900f5bb00ec199
Rhett Flanagan <rhettsfp@gmail.com>
2020-08-31 16:19:06 +0200

    Fixed a missing bracer

--- src/main/java/intersectionmanagement/simulator/car/Car.java
+++ src/main/java/intersectionmanagement/simulator/car/Car.java
@@ -42,8 +42,8 @@ public class Car extends Actor {
         createSensorRow(0.93f, 50, 16);
         createSensorRow(-1.25f, 45, 16);
         createSensorRow(1.25f, 45, 16);
-        createSensorRow((float)(0.5*Math.PI), 40, 12);
-        createSensorRow((float)(-0.5*Math.PI), 40, 12);
+        createSensorRow((float) (0.5 * Math.PI), 40, 12);
+        createSensorRow((float) (-0.5 * Math.PI), 40, 12);
         createSensorRow(1.925f, 35, 12);
         createSensorRow(-1.925f, 35, 12);
         createSensorRow(2.3f, 35, 12);
@@ -67,6 +67,78 @@ public class Car extends Actor {
 //        createSensorRow(-2.5f, 32, 12);
     }
 
+    public Car(Simulator simulator, Node firstTarget, CarController controller, int sensorConfig) {
+        super(simulator, firstTarget);
+        speed = 0.5f;
+        radius = 1.0f;
+        solid = true;
+        turnControl = 0;
+        wheelbase = 4;
+
+        this.controller = controller;
+        this.inactiveController = new HeuristicController();
+        controllerActive = false;
+        sensorArray = new ArrayList<>();
+
+        if (sensorConfig == 0) {
+            // Default Sensor Config
+            createSensorRow(0, 60, 24);
+            createSensorRow((float) Math.PI, 30, 12);
+            createSensorRow(-0.3f, 55, 16);
+            createSensorRow(0.3f, 55, 16);
+            createSensorRow(-0.65f, 50, 16);
+            createSensorRow(0.65f, 50, 16);
+            createSensorRow(-1.1f, 45, 16);
+            createSensorRow(1.1f, 45, 16);
+            createSensorRow((float) (0.5 * Math.PI), 40, 16);
+            createSensorRow((float) (-0.5 * Math.PI), 40, 16);
+            createSensorRow(2, 35, 12);
+            createSensorRow(-2, 35, 12);
+            createSensorRow(2.5f, 32, 12);
+            createSensorRow(-2.5f, 32, 12);
+            createSensorRow(-2.7f, 32, 12);
+        } else if (sensorConfig == 1) {
+            // New sensor setup, 0.3 radians seems to provide near optimal coverage for the turning + increased pedestrian flow
+            // Reduced coverage on the rear
+            createSensorRow(0, 60, 24);
+            createSensorRow((float) Math.PI, 30, 12);
+            createSensorRow(-0.3f, 55, 16);
+            createSensorRow(0.3f, 55, 16);
+            createSensorRow(-0.625f, 52, 16);
+            createSensorRow(0.625f, 52, 16);
+            createSensorRow(-0.93f, 50, 16);
+            createSensorRow(0.93f, 50, 16);
+            createSensorRow(-1.25f, 45, 16);
+            createSensorRow(1.25f, 45, 16);
+            createSensorRow((float) (0.5 * Math.PI), 40, 12);
+            createSensorRow((float) (-0.5 * Math.PI), 40, 12);
+            createSensorRow(1.925f, 35, 12);
+            createSensorRow(-1.925f, 35, 12);
+            createSensorRow(2.3f, 35, 12);
+            createSensorRow(-2.3f, 35, 12);
+            createSensorRow(2.7f, 32, 12);
+        } else if (sensorConfig == 2) {
+            //  Full coverage sensor array
+            createSensorRow(0, 60, 24);
+            createSensorRow((float) Math.PI, 30, 12);
+            createSensorRow(-0.3f, 55, 16);
+            createSensorRow(0.3f, 55, 16);
+            createSensorRow(-0.625f, 52, 16);
+            createSensorRow(0.625f, 52, 16);
+            createSensorRow(-0.93f, 50, 16);
+            createSensorRow(0.93f, 50, 16);
+            createSensorRow(-1.25f, 45, 16);
+            createSensorRow(1.25f, 45, 16);
+            createSensorRow((float) (0.5 * Math.PI), 40, 12);
+            createSensorRow((float) (-0.5 * Math.PI), 40, 12);
+            createSensorRow(1.925f, 35, 12);
+            createSensorRow(-1.925f, 35, 12);
+            createSensorRow(2.3f, 35, 12);
+            createSensorRow(-2.3f, 35, 12);
+            createSensorRow(2.7f, 32, 12);
+            createSensorRow(-2.7f, 32, 12);
+        }
+    }
 //    public Car(Simulator simulator, Node firstTarget, CarController controller, int sensors) {
 //        super(simulator, firstTarget);
 //        speed = 0.5f;
@@ -131,9 +203,9 @@ public class Car extends Actor {
             controls = inactiveController.getControls(getSensorValues(actorArray, 1));
         }
         targetSpeed = (float) controls[0];
-        turnControl = (float) ((controls[1]*2)-1)*(Utility.CAR_TURN_MAX*0.15f);
+        turnControl = (float) ((controls[1] * 2) - 1) * (Utility.CAR_TURN_MAX * 0.15f);
 
-        float acceleration = limitAcceleration(targetSpeed*Utility.CAR_SPEED_MAX - speed);
+        float acceleration = limitAcceleration(targetSpeed * Utility.CAR_SPEED_MAX - speed);
 
         speed += acceleration;
         limitSpeed();
@@ -171,7 +243,7 @@ public class Car extends Actor {
         for (int i = 0; i < quantity; i++) {
             // Add one to i because we want sensors starting off the car and ending at final distance
             float sensorDistance = (distance / quantity) * (i + 1);
-            sensorRow.add(new Sensor(sensorDistance, angle, sensorDistance*0.15f));
+            sensorRow.add(new Sensor(sensorDistance, angle, sensorDistance * 0.15f));
         }
         sensorArray.add(sensorRow);
     }
@@ -195,7 +267,7 @@ public class Car extends Actor {
             List<Sensor> sensorRow = sensorArray.get(i);
             for (Sensor sensor : sensorRow) {
                 if (sensor.activated(this, actorArray)) {
-                    float value = 1 - ((sensorRow.indexOf(sensor)*1.0f) / sensorRow.size());
+                    float value = 1 - ((sensorRow.indexOf(sensor) * 1.0f) / sensorRow.size());
                     sensorValues[i] = value;
                     if (maxSensors == 1) {
                         sensorValues[1] = sensor.lastSpeed;
